16ac1d291bd8d33ef58479a81035376da7893ddb
Eng.Ola Bakheet <olabakheet06@gmail.com>
2025-06-05 01:13:05 +0300

    Create Dictionary.py

---
+++ Data Types/Dictionary.py
@@ -0,0 +1,14 @@
+# example 1
+my_dict = {}                               # create an empty dictionary
+my_dict['one'] = 43                         # my_dict[key] = value    
+my_dict[2] = "Hello coders"
+print(my_dict)
+tiny_dict = {'name': 'rola','code':6734, 'major': 'art'}
+
+print()
+print (my_dict['one'])                           # Prints value for 'one' key
+print (my_dict[2])                               # Prints value for 2 key
+
+print (tiny_dict)                                # Prints complete dictionary
+print (tiny_dict.keys())                         # Prints all the keys
+print (tiny_dict.values())                        # Prints all the values

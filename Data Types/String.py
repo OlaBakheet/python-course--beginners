6ffa37a11f53e87fca618ac9a236abba3b212230
Eng.Ola Bakheet <olabakheet06@gmail.com>
2025-06-05 01:07:25 +0300

    Create String.py

---
+++ Data Types/String.py
@@ -0,0 +1,22 @@
+# String data types with Slicing
+# immutable
+# We count the space
+# example 1
+
+print('Welcome to python Basics Course')
+
+print("Welcome to python Basics Course")
+
+print('''Welcome to python Basics Course''')
+
+
+****************************************************************************************
+# example 2
+str = 'Hello Coders!'
+
+print (str)                      # Prints complete string
+print (str[0])                   # Prints first character of the string
+print (str[2:7])                 # Prints characters starting from 3rd to 7th
+print (str[2:])
+print (str * 2)                  # Prints string two times
+print (str + "You are awsome")   # Prints concatenated string

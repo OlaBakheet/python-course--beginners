5e62b5cf4c1f9a6b30c551028678613965307f7c
Eng.Ola Bakheet <olabakheet06@gmail.com>
2025-06-05 01:17:30 +0300

    Create bytearray.py

---
+++ Data Types/Binary/bytearray.py
@@ -0,0 +1,11 @@
+# Mutable
+# example 1
+value = bytearray([80, 121, 116, 104, 111, 110])
+print(value)                # output: Python
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# example 2
+val = bytearray("Python",'utf-8')
+print(val)                  # output: bytearray(b'Python')
